c277824d8e89f0828f0feb92c1df94565621e13c
Oskar Wickström <oskar@wickstrom.tech>
2026-03-06 16:03:09 +0100

    more cleanup

--- basic.ts
+++
@@ -1,9 +0,0 @@
-import { always, extract } from "@antithesishq/bombadil";
-export * from "@antithesishq/bombadil/defaults";
-
-const h1 = extract(state => {
-  const element = state.document.querySelector("h1");
-  return { text: element?.textContent ?? "" };
-});
-
-export const has_titles = always(() => h1.current.text.trim() !== "");
